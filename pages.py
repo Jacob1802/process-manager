5f603963946dc702fb6868e640db83ea7afaf221
unknown <Jacobrdilonardo@gmail.com>
2024-06-17 16:30:37 +1000

    move repeat methods to BasePage, implement LockServicePage & EditServicePage

--- pages.py
+++ pages.py
@@ -1,7 +1,8 @@
+import datetime as dt
 import tkinter as tk
 import subprocess
-import utils
 import logging
+import utils
 import sys
 
 logging.basicConfig(
@@ -15,8 +16,6 @@ CONFIG_FILE = 'config.json'
 class BasePage(tk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
-
-        # self.config = utils.load_config(CONFIG_FILE)
         self.controller = controller
 
         # Configure the grid layout
@@ -38,6 +37,66 @@ class BasePage(tk.Frame):
         # Add padding to the top by configuring an empty row at the top
         self.grid_rowconfigure(0, minsize=10)
     
+    def start_service_logic(self, service_name, start_time, end_time, days):
+        try:
+            if not self.check_time_entry(start_time):
+                return
+            if not self.check_time_entry(end_time):
+                return
+            day_list = self.check_day_entry(days)
+            if not day_list:
+                return
+
+            self.controller.config[service_name] = {
+                "start_time": start_time,
+                "end_time": end_time,
+                "days": day_list
+            }
+
+            utils.save_config(CONFIG_FILE, self.controller.config)
+            utils.create_service(service_name, sys.executable)
+            tk.messagebox.showinfo("Info", "Service started successfully")
+        except Exception as e:
+            tk.messagebox.showerror("Error", f"Failed to start service: {e}")
+
+    def check_time_entry(self, time):
+        if ":" not in time:
+            tk.messagebox.showerror("Error", "Missing semi colon")
+            return False
+        time_str = time.split(':')
+        if not time_str[0].isnumeric() or not time_str[1].isnumeric():
+            tk.messagebox.showerror("Error", "Invalid time")
+            return False
+
+        return True
+
+    def check_day_entry(self, days_string):
+        days = []
+        for day in days_string.split(','):
+            if day.strip().lower() not in ['monday', 'tuesday', 'wednesday', 'thrusday', 'friday', 'saturday', 'sunday']:
+                tk.messagebox.showerror("Error", "Invalid day")
+                return
+            else: days.append(day)
+        
+        return days
+
+    def validate_service_name(self, service_name):
+        if not service_name:
+            tk.messagebox.showerror("Error", "Service name cannot be empty")
+            return False
+    
+        if service_name not in self.controller.config.keys():
+            tk.messagebox.showerror("Error", "Service does not exist")
+            return False
+        
+        if service_name in self.controller.locked_config.keys():
+            # Calculate time left, if time left throw error
+            if remaining_time := self.calculate_remaining_time(service_name):
+                tk.messagebox.showerror("Error", f"Service is locked for {remaining_time} more hours")
+                return False
+        
+        return True
+
     def add_service_page_header(self, text):
         tk.Label(self, text=text).grid(row=0, column=1, columnspan=2, pady=10)
 
@@ -52,7 +111,20 @@ class BasePage(tk.Frame):
 
     def add_back_to_home_button(self):
         tk.Button(self, text="Back to Home", command=lambda: self.controller.show_frame("HomePage")).grid(row=10, column=1, columnspan=2, pady=10)
+    
+    def check_if_service_locked(self, service_name):
+        start_lock, duration = self.controller.locked_config[service_name]
+        return True if (start_lock + duration) > dt.datetime.now().timestamp() else False 
 
+    def calculate_remaining_time(self, service_name):
+        if self.check_if_service_locked(service_name):
+            start_time, duration = self.controller.locked_config[service_name]
+            remaining_time = (dt.datetime.now().timestamp() - (start_time + duration)) / 60 / 60
+            return remaining_time
+        # Release lock
+        del self.controller.locked_config[service_name]
+        return None
+    
 class HomePage(BasePage):
     def __init__(self, parent, controller):
         super().__init__(parent, controller)
@@ -98,21 +170,7 @@ class StartServicePage(BasePage):
     def __init__(self, parent, controller):
         super().__init__(parent, controller)
 
-        # Configure the grid layout
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_rowconfigure(1, weight=1)
-        self.grid_rowconfigure(2, weight=1)
-        self.grid_rowconfigure(3, weight=1)
-        self.grid_rowconfigure(4, weight=1)
-        self.grid_rowconfigure(5, weight=1)
-        self.grid_rowconfigure(6, weight=1)
-        self.grid_rowconfigure(7, weight=1)
-        self.grid_rowconfigure(8, weight=1)
-        self.grid_rowconfigure(9, weight=1)
-        self.grid_rowconfigure(10, weight=1)
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_columnconfigure(2, weight=1)
+        # Add to the grid layout
         self.grid_columnconfigure(3, weight=1)
 
         # Add the widgets using grid
@@ -137,73 +195,29 @@ class StartServicePage(BasePage):
         self.days_entry = tk.Entry(self)
         self.days_entry.grid(row=4, column=2, pady=0, sticky="w")
 
-        self.add_submit(self.name_service)
+        self.add_submit(self.start_service)
         self.add_back_to_home_button()
     
-    def name_service(self):
+    def start_service(self):
         service_name = self.service_name_entry.get()
+
+        if not service_name:
+            tk.messagebox.showerror("Error", "Service name cannot be empty")
+            return
+
+        if service_name in self.controller.config.keys():
+            tk.messagebox.showerror("Error", "Service already exists")
+            return
+        
         start_time = self.start_time_entry.get()
         end_time = self.end_time_entry.get()
         days = self.days_entry.get()
         
-        # Here you can add the logic to handle the form submission
-        # For example, saving the data to a config file, validating input, etc.
-        if service_name and start_time and end_time and days:
+        if start_time and end_time and days:
             self.start_service_logic(service_name, start_time, end_time, days)
             self.controller.show_frame("HomePage")
         else:
             tk.messagebox.showerror("Error", "All fields are required!")
-
-    def start_service_logic(self, service_name, start_time, end_time, days):
-        try:
-            if not service_name:
-                tk.messagebox.showerror("Error", "Service name cannot be empty")
-                return
-
-            if service_name in self.controller.config.keys():
-                tk.messagebox.showerror("Error", "Service already exists")
-                return
-
-            if not self.check_time_entry(start_time):
-                return
-            if not self.check_time_entry(end_time):
-                return
-            day_list = self.check_day_entry(days)
-            if not day_list:
-                return
-
-            self.controller.config[service_name] = {
-                "start_time": start_time,
-                "end_time": end_time,
-                "days": day_list
-            }
-
-            utils.save_config(CONFIG_FILE, self.controller.config)
-            utils.create_service(service_name, sys.executable)
-            tk.messagebox.showinfo("Info", "Service started successfully")
-        except Exception as e:
-            tk.messagebox.showerror("Error", f"Failed to start service: {e}")
-
-    def check_time_entry(self, time):
-        if ":" not in time:
-            tk.messagebox.showerror("Error", "Missing semi colon")
-            return False
-        time_str = time.split(':')
-        if not time_str[0].isnumeric() or not time_str[1].isnumeric():
-            tk.messagebox.showerror("Error", "Invalid time")
-            return False
-
-        return True
-
-    def check_day_entry(self, days_string):
-        days = []
-        for day in days_string.split(','):
-            if day.strip().lower() not in ['monday', 'tuesday', 'wednesday', 'thrusday', 'friday', 'saturday', 'sunday']:
-                tk.messagebox.showerror("Error", "Invalid day")
-                return
-            else: days.append(day)
-        
-        return days
 
 class StopServicePage(BasePage):
     def __init__(self, parent, controller):
@@ -216,13 +230,7 @@ class StopServicePage(BasePage):
         
     def stop_service(self):
         service_name = self.service_name_entry.get()
-        
-        if not service_name:
-            tk.messagebox.showerror("Error", "Service name cannot be empty")
-            return
-
-        if service_name not in self.controller.config.keys():
-            tk.messagebox.showerror("Error", "Service does not exist")
+        if not self.validate_service_name(service_name):
             return
 
         try:
@@ -243,15 +251,9 @@ class DeleteServicePage(BasePage):
         
     def delete_service(self):
         service_name = self.service_name_entry.get()
-        
-        if not service_name:
-            tk.messagebox.showerror("Error", "Service name cannot be empty")
+        if not self.validate_service_name(service_name):
             return
-    
-        if service_name not in self.controller.config.keys():
-            tk.messagebox.showerror("Error", "Service does not exist")
-            return
-    
+
         try:
             subprocess.run([self.controller.nssm, 'remove', service_name])
             del self.controller.config[service_name]
@@ -267,7 +269,31 @@ class LockServicePage(BasePage):
         
         self.add_service_page_header(text="Lock Service Page")
         self.add_service_name_input()
+        self.length_label = tk.Label(self, text="Length (Hrs):")
+        self.length_label.grid(row=2, column=1, pady=0, sticky="e")
+
+        self.length_entry = tk.Entry(self)
+        self.length_entry.grid(row=2, column=2, pady=0, sticky="w")
+        self.add_submit(self.lock_service)
         self.add_back_to_home_button()
+
+    def lock_service(self):
+        service_name = self.service_name_entry.get()
+        if not self.validate_service_name(service_name):
+            return
+        
+        length = self.length_entry.get()
+        # Check if length is valid
+        if not length.isnumeric():
+            tk.messagebox.showerror("Error", "Invalid time")
+
+        self.controller.locked_config[service_name] = {
+            "start_time": dt.datetime.now().timestamp(),
+            "length_seconds": int(length) * 60 * 60
+        }
+        
+        tk.messagebox.showinfo("Info", f"Service successfully locked for {length} hours")
+        self.controller.show_frame("HomePage")
 
 class EditServicePage(BasePage):
     def __init__(self, parent, controller):
@@ -275,4 +301,39 @@ class EditServicePage(BasePage):
         
         self.add_service_page_header(text="Edit Service Page")
         self.add_service_name_input()
+
+        self.start_time_label = tk.Label(self, text="Start Time (HH:MM):")
+        self.start_time_label.grid(row=2, column=1, pady=0, sticky="e")
+
+        self.start_time_entry = tk.Entry(self)
+        self.start_time_entry.grid(row=2, column=2, pady=0, sticky="w")
+
+        self.end_time_label = tk.Label(self, text="End Time (HH:MM):")
+        self.end_time_label.grid(row=3, column=1, pady=0, sticky="e")
+
+        self.end_time_entry = tk.Entry(self)
+        self.end_time_entry.grid(row=3, column=2, pady=0, sticky="w")
+
+        self.days_label = tk.Label(self, text="Days (e.g. Monday, Tuesday, Sunday):")
+        self.days_label.grid(row=4, column=1, pady=0, sticky="e")
+
+        self.days_entry = tk.Entry(self)
+        self.days_entry.grid(row=4, column=2, pady=0, sticky="w")
+
+        self.add_submit(self.edit_service_process)
         self.add_back_to_home_button()
+    
+    def edit_service_process(self):
+        service_name = self.service_name_entry.get()
+        if not self.validate_service_name(service_name):
+            return
+        
+        start_time = self.start_time_entry.get()
+        end_time = self.end_time_entry.get()
+        days = self.days_entry.get()
+        
+        if start_time and end_time and days:
+            self.start_service_logic(service_name, start_time, end_time, days)
+            self.controller.show_frame("HomePage")
+        else:
+            tk.messagebox.showerror("Error", "All fields are required!")
