9b7cf11e555dbe48e8cb402e34ed57d54b7e2fe1
unknown <Jacobrdilonardo@gmail.com>
2024-06-18 21:21:04 +1000

    fix paths, clean up file

--- utils.py
+++ utils.py
@@ -1,53 +1,56 @@
 import subprocess
+import requests
+import zipfile
 import logging
 import psutil
 import json
 import os
 
-def is_gamecloser_running(service_name):
-    """Check if the GameCloser service is running."""
-    try:
-        result = subprocess.run(['sc', 'query'], capture_output=True, text=True, check=True)
-        lines = result.stdout.split('\n')
-        for line in lines:
-            if service_name in line:
-                return True
-    except subprocess.CalledProcessError as e:
-        logging.error(f"Error occurred: {e}")
-    return False
+appdata_dir = os.path.join(os.environ['APPDATA'], 'ProcessCloserService')
+zip_path = os.path.join(appdata_dir, 'nssm-2.24.zip')
+nssm_path = os.path.join(appdata_dir, 'nssm-2.24', 'win64', 'nssm.exe')
+log_path = os.path.join(appdata_dir, 'output.log')
+
+
+logging.basicConfig(filename=log_path, level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def check_and_install_nssm():
+    """Check if NSSM is installed in System32 and install it if not."""
+    if not os.path.exists(nssm_path):
+        download_nssm()
+        extract_zip(zip_path, appdata_dir)
+        # Remove the downloaded zip file after extraction
+        os.remove(zip_path)
+
+def download_nssm():
+    """Helper function to download a file from a URL to a local path."""
+    download_url = "https://nssm.cc/release/nssm-2.24.zip"
+    with requests.get(download_url, stream=True) as r:
+        r.raise_for_status()
+        with open(zip_path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+
+def extract_zip(zip_filepath, extract_to):
+    """Extract a zip file to a specified directory."""
+    with zipfile.ZipFile(zip_filepath, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
 
 
 def create_service(service_name, executable):
-    # WORKING_DIRECTORY = os.path.join(os.path.dirname(os.path.abspath(__file__)), "game_closer.py")
-    dest_folder = get_dest_folder()
-    # log_path = os.path.join(dest_folder, 'Logs', 'output.log')
-
-    nssm_path = os.path.join(dest_folder, 'nssm-2.24\\win64\\nssm.exe')
     if not nssm_path:
-        print("Error: Path to NSSM not provided.")
+        logging.error("Error: Path to NSSM not provided.")
         return
     
-    # exe_path = os.path.join(dest_folder, 'game_closer.exe')
-    exe_path = os.path.join(dest_folder, 'C:\\Users\\user\\Documents\\Projects\\process-manager\\close.py')
+    exe_path = 'C:\\Users\\user\\Documents\\Projects\\process-manager\\close.py'
 
     try:
         subprocess.run([nssm_path, 'install', service_name, executable, exe_path, service_name])
-        subprocess.run([nssm_path, 'set', service_name, 'AppStdout', 'C:\\Users\\user\\Documents\\Projects\\process-manager\\output.log'], check=True)
-        subprocess.run([nssm_path, 'set', service_name, 'AppStderr', 'C:\\Users\\user\\Documents\\Projects\\process-manager\\output.log'], check=True)
-        # Set the working directory
-        # subprocess.run([nssm_path, "set", service_name, "AppDirectory", WORKING_DIRECTORY], check=True)
-
+        subprocess.run([nssm_path, 'set', service_name, 'AppStdout', log_path], check=True)
+        subprocess.run([nssm_path, 'set', service_name, 'AppStderr', log_path], check=True)
         subprocess.run([nssm_path, 'start', service_name], check=True)
-        print(f"Service '{service_name}' started successfully.")
-
     except Exception as e:
-        print(f"Error installing service: {e}")
-
-
-def get_dest_folder():
-    parent = os.path.dirname(os.getcwd())
-    return os.path.join(parent, 'Downloads', 'game-manager')
-
+        logging.error(f"Error installing service: {e}")
 
 def load_config(config_file):
     if os.path.exists(config_file):
@@ -56,11 +59,9 @@ def load_config(config_file):
     
     return {}
 
-
 def save_config(config_file, config):
     with open(config_file, "w") as f:
         json.dump(config, f, indent=4)
-
 
 def convert_days_to_nums(days):
     day_map = {
@@ -74,7 +75,6 @@ def convert_days_to_nums(days):
     }
     # Convert each day word to num
     return [day_map[day] for day in days]
-
 
 def get_available_processes():
     return {proc.info['name'].lower(): proc for proc in psutil.process_iter(['name']) if '.exe' in proc.info['name'].lower()}
